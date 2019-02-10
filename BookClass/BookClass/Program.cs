c0dfd51f76b72a2a807ddcbb0fa15d94fb04879c
tapiosir <tapio.sirvio11@gmail.com>
2019-02-10 14:58:31 +0200

    made solution started working w/ tasks

---
+++ BookClass/BookClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BookClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book testBook = new Book("kirja1", "kiva kirjailia","01",14);
+            Book testBook1 = new Book("kirja2", "kiva kirjailia", "01", 15);
+
+            Console.WriteLine(testBook.GetInfo());
+            Console.WriteLine(testBook1.GetInfo());
+
+            Console.WriteLine(testBook.ComparePrice(testBook1));
+        }
+    }
+}
---
+++ BookClass/BookClass/book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookClass
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public string bookId;
+        public double price;
+        private readonly string skippy = "----------------------------------------------------------";
+
+        public Book(string title, string author, string bookId, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.bookId = bookId;
+            this.price = price;
+        }
+
+        public string GetInfo()
+        {
+            return $" Name: {this.title}\n Author: {this.author}\n Price: {this.price}\n Book id: {this.bookId} \n {skippy}";
+
+        }
+
+        public string ComparePrice(Book book)
+        {
+            if (price > book.price)
+            {
+                return $" {title} is more expensive than {book.title}";
+            }
+            else if (price < book.price)
+            {
+                return $" {book.title} is more expensive than {title}";
+            }
+            else
+            {
+                return $" books costs same";
+            }
+        }
+    }
+}
